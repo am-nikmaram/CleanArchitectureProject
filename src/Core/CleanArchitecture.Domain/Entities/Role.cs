d0cd5eb3c9c1273bb2567c32ec869d523e522a2f
am-nikmaram <am.nikmaram@gmail.com>
2025-02-16 14:38:03 -0500

    after DbContext

---
+++ src/Core/CleanArchitecture.Domain/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Common;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Role : IdentityRole<int>, IEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+
+
+}
---
+++ src/Core/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitecture.Domain.Common;
+using CleanArchitecture.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class User: IdentityUser<int>,IEntity<int>
+    {
+        public User()
+        {
+            IsActive = true;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset? LastLoginDate { get; set; }
+
+    }
+}
