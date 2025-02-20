b855c9f197814cf0122000e17c34fbba9e6c36d2
am-nikmaram <am.nikmaram@gmail.com>
2025-02-19 19:05:03 -0500

    After WebFramework

---
+++ src/Shared/CleanArchitecture.Shared/Extensions/RegExHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CleanArchitecture.Shared.Extensions;
+
+public static class RegExHelpers
+{
+    public static bool MatchesApiVersion(string apiVersion, string text)
+    {
+        string pattern = $@"(?<=\/|^){Regex.Escape(apiVersion)}(?=\/|$)";
+
+        Regex exactMatchRegex = new Regex(pattern, RegexOptions.Compiled);
+
+        return exactMatchRegex.IsMatch(text);
+    }
+}
