4a965a2e0ed3eecf70e3d7c34764778182e82ef7
am-nikmaram <am.nikmaram@gmail.com>
2025-02-22 11:45:55 -0500

    before migration DB

---
+++ src/Infrastructure/CleanArchitecture.Infrastructure.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace CleanArchitecture.Infrastructure.Persistence;
+
+public static class ServiceCollectionExtensions
+{
+    #region Newmethod
+
+
+    //public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    //{
+
+    //    services.AddDbContext<ApplicationDbContext>(options =>
+    //    {
+    //        options
+    //            .UseSqlServer(configuration.GetConnectionString("SqlServer"));
+    //    });
+
+    //    return services;
+    //}
+
+    //public static async Task ApplyMigrationsAsync(this  WebApplication app)
+    //{
+    //    await using var scope = app.Services.CreateAsyncScope();
+    //    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+    //    if (context is null)
+    //        throw new Exception("Database Context Not Found");
+
+    //    await context.Database.MigrateAsync();
+    //}
+    #endregion
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+        });
+        return services;
+    }
+
+    public static async Task ApplyMigrationsAsync(this IApplicationBuilder app)
+    {
+        await using var scope = app.ApplicationServices.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+        if (context is null)
+            throw new Exception("Database Context Not Found");
+
+        await context.Database.MigrateAsync();
+    }
+
+
+}
--- src/Presentation/CleanArchitecture.Presentation/Program.cs
+++ src/Presentation/CleanArchitecture.Presentation/Program.cs
@@ -1,25 +1,118 @@
+using Carter;
+using CleanArc.Application.ServiceConfiguration;
+using CleanArc.Web.Api.Controllers.V1.UserManagement;
+using CleanArchitecture.Application.Models.ApiResult;
+using CleanArchitecture.Domain.Entities.User;
+using CleanArchitecture.Infrastructure.Identity;
+using CleanArchitecture.Infrastructure.Identity.Dtos;
+using CleanArchitecture.Infrastructure.Identity.Jwt;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Shared.Extensions;
+using CleanArchitecture.WebFramework;
+using CleanArchitecture.WebFramework.Filters;
+using CleanArchitecture.WebFramework.Middlewares;
+using CleanArchitectureWebFramework.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+builder.Services.Configure<IdentitySettings>(configuration.GetSection(nameof(IdentitySettings)));
+var identitySettings = configuration.GetSection(nameof(IdentitySettings)).Get<IdentitySettings>();
+
+
+builder.Services.AddControllers(options =>
+{
+options.Filters.Add(typeof(OkResultAttribute));
+options.Filters.Add(typeof(NotFoundResultAttribute));
+options.Filters.Add(typeof(ContentResultFilterAttribute));
+options.Filters.Add(typeof(ModelStateValidationAttribute));
+options.Filters.Add(typeof(BadRequestResultFilterAttribute));
+options.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult<Dictionary<string, List<string>>>),
+    StatusCodes.Status400BadRequest));
+options.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+    StatusCodes.Status401Unauthorized));
+options.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+    StatusCodes.Status403Forbidden));
+options.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+    StatusCodes.Status500InternalServerError));
+
+
+}).ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+    options.SuppressMapClientErrors = true;
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwagger("v1", "v1.1");
+
+builder.Services.AddCarter(configurator: configurator => { configurator.WithEmptyValidators(); });
+
+
+builder.Services.AddApplicationServices()
+    .RegisterIdentityServices(identitySettings)
+    .AddPersistenceServices(configuration)
+    .AddWebFrameworkServices();
+
+builder.Services.RegisterValidatorsAsServices();
+builder.Services.AddExceptionHandler<ExceptionHandler>();
+
+builder.Services.AddAutoMapper(expression =>
+{
+    expression.AddMaps(typeof(User), typeof(JwtService), typeof(UserController));
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+await app.ApplyMigrationsAsync();
+await app.SeedDefaultUsersAsync();
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseExceptionHandler(_ => { });
+app.UseSwaggerAndUi();
 
+app.MapCarter();
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+
+await app.RunAsync();
+
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
